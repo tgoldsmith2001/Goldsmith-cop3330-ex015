b15977dcb66b049d31a3a76a5b74f9fcd4c32895
HP <tgoldsmith01@yahoo.com>
2021-08-30 09:33:46 -0400

    Newest Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@
  *  Copyright 2021 Tyler Goldsmith
  */
 import java.util.Scanner;
-import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
