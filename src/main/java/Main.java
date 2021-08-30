803b7936db400f75de03e76ad77659a85c86d8d6
HP <tgoldsmith01@yahoo.com>
2021-08-30 09:33:06 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Tyler Goldsmith
+ */
+import java.util.Scanner;
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String Password="Object_Programming";
+        System.out.print("Enter your Username: ");
+        String Str1 = input.nextLine();
+        System.out.print("Enter your Password: ");
+        String Str2 = input.nextLine();
+        if(Str2.equals(Password))
+            System.out.println("Welcome!");
+        else
+            System.out.println("I don't know you");
+    }
+}
